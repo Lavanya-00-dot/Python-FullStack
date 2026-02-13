0dc02fc81426db88a0a0119e4358cddf3c989506
Lavanya-00-dot <boddylavanya@gmail.com>
2026-02-13 09:10:58 +0530

    Add files via upload

---
+++ Day1.py
@@ -0,0 +1,14 @@
+Python 3.14.3 (tags/v3.14.3:323c59a, Feb  3 2026, 16:04:56) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> print("i am lavanya")
+i am lavanya
+>>> print("i started my python classes")
+i started my python classes
+>>> print("this is my day 1")
+this is my day 1
+>>> print("i learned Feature,Python Installation)
+...       
+SyntaxError: unterminated string literal (detected at line 1)
+>>> print("i learned Features,Python Installation")
+...       
+i learned Features,Python Installation

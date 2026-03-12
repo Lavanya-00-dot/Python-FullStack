6eb3dfd1a24cfdc52d038d8b3425b1be8e78cc68
Lavanya-00-dot <boddylavanya@gmail.com>
2026-03-12 11:45:14 +0530

    Add files via upload

---
+++ Day6 [ slicing ].py
@@ -0,0 +1,11 @@
+s = "codegnan"
+print("1", s [5:0:-1])    #finite slice
+print("2", s [5:8:-1])
+print("3", s [5:100:-1])  #infinite slice
+print("4", s [ : :-1])
+print("4", s [0:5: ])
+print("5", s [: :2])
+print("6", s [-2:5:1])    #convert to step type
+print("7", s [6:5:1])
+
+
